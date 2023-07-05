57c9a411efd8be389d2ebd7758e6948d349438b2
Mokalol25 <93078882+Mokalol25@users.noreply.github.com>
2023-07-05 15:17:08 +0000

    Deploying to gh-pages from @ Mokalol25/FoodSta@d5aa3b33c92e677fa23c9fb921b4d1b67915e0f1 🚀

--- index.059d3196.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},devDependencies:{serve:"^11.2.0"},main:"index.html",name:"swiper-navigation",scripts:{build:"echo This is a static template, there is no bundler or bundling involved!",start:"serve"},tags:["swiper"]});
-//# sourceMappingURL=index.059d3196.js.map
---
+++ index.5fc5f879.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper-container",{slidesPerView:1,spaceBetween:0,centeredSlides:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},loop:!0,autoplay:{delay:3e3,disableOnInteraction:!0}});
+//# sourceMappingURL=index.5fc5f879.js.map
