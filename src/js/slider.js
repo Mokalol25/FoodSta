27a904bc49e5c50982809c4e10bf7ad5e853abaf
Mokalol25 <marko.zhoba@gmail.com>
2023-07-05 17:46:16 +0300

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,8 +7,8 @@
 //     clickable: true,
 //   },
 //   navigation: {
-//     nextEl: '.right-slider',
-//     prevEl: '.left-slider',
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
 //   },
 //   loop: true,
 //   autoplay: {
@@ -16,3 +16,20 @@
 //     disableOnInteraction: true,
 //   },
 // });
+var swiper = new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  devDependencies: {
+    serve: '^11.2.0',
+  },
+  main: 'index.html',
+  name: 'swiper-navigation',
+  scripts: {
+    build:
+      'echo This is a static template, there is no bundler or bundling involved!',
+    start: 'serve',
+  },
+  tags: ['swiper'],
+});
